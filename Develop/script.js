966b3504bc6f45d0783ea76b39ed1b97dea645bc
Emily Ciarabellini <136280931+ECiarabellini@users.noreply.github.com>
2023-12-08 15:36:05 -0800

    Add code to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -10,7 +10,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -20,7 +19,10 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword() {
   // prompt for user for password length: 8 - 128 characters
   var verification;    
-  var pwdLength = prompt("Specify the length of the password needed between 8 - 128.");
+  var pwdLength = 0;
+  while (pwdLength < 8 || pwdLength > 128){
+    pwdLength= prompt("Specify the length of the password needed between 8 - 128.");
+  } 
 
   while (!verification) {
     var allowLowerCase = prompt("Include lowercase letters? Click OK for Yes, Click Cancel for No");
@@ -29,49 +31,41 @@ function generatePassword() {
     var allowSpecialChars = prompt("Include special characters? Click OK for Yes, Click Cancel for No");  
     
     // validate at least one of the 4 types was selected, if not, ask again
-    if (allowLowerCase==="" || allowUpperCase==="" || allowNumeric==="" || allowSpecialChars===""){
+    if (allowLowerCase != null || allowUpperCase != null || allowNumeric != null || allowSpecialChars != null){
       verification = 1;
     } else {
         prompt("Must click OK for at least one option. Try again.");
       }
   }
 
-  // create arrays of each type: ie [1,2,3,4,5,6,7] 
+  // create arrays of each character type
   var lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //26 length
   var uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; //26 length
   var numerics = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; //10 length
   var specialChars = ['!', '”', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']; //30 legth
   
-  // generate password that matches criteria:
-  //   - exclude any array that user selected NO to 
-  
+  // combine arrays, excluding any array that user selected NO to 
+  var combinedArray = [];
+  if (allowLowerCase != null) {
+    combinedArray = combinedArray.concat(lowers);
+  }
+  if (allowUpperCase != null) {
+    combinedArray = combinedArray.concat(uppers);
+  }
+  if (allowNumeric != null) {
+    combinedArray = combinedArray.concat(numerics);
+  }
+  if (allowSpecialChars != null) {
+    combinedArray = combinedArray.concat(specialChars);
+  }
 
+  //   random number to select which array item to add to string of generated password
+  var generatedPwd = "";
+  for (let x=0; x < pwdLength; x++){
+    randomDigit = Math.floor(Math.random() * (combinedArray.length));
+    generatedPwd = generatedPwd + combinedArray[randomDigit];
+  };
 
-
-
-  //   - random number to select which array to pick from (length of how many YESes... increment during dialogue), 
-  //    then random number length of array items to select item in array, save to password
-
-
-
-
-
-
-  console.log('pwdLength is ', pwdLength);
-  console.log('allowLowerCase is ', allowLowerCase);
-  console.log('allowUpperCase is ', allowUpperCase);
-  console.log('allowNumeric is ', allowNumeric);
-  console.log('allowSpecialChars is ', allowSpecialChars);
-  // console.log(verification);
-  // console.log(!verification);
-  console.log(specialChars.length);
-
-
-
-
-
-
-
-  return password;
+  return generatedPwd;
 
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password Generator Starter Code
+
+sourced random line from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
